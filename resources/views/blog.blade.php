890379343c6fe58d3b0c2d841f4a50be658fc7c6
MrPxndBigCode <chillyboy2547@gmail.com>
2026-06-29 16:19:58 +0700

    update

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,15 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ยินดีต้อนรับ </title>
+    <title>บลอก</title>
 </head>
 
 <body>
-    <h2>ยินดีต้อดนรับ </h2>
+    <h2>เกี่ยวกับพระ </h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus facilis quas nisi a ut ipsa officia rem officiis
         possimus ex est et, expedita ad reprehenderit mollitia unde. Accusamus, minus perferendis.</p>
-    <a href="about">about</a>
-    <a href="blog">blog</a>
 </body>
 
 </html>
