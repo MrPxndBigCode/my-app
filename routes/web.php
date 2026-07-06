f67615d0692517eb62c91604ccbbd411eb1e5167
MrPxndBigCode <chillyboy2547@gmail.com>
2026-07-06 16:08:12 +0700

    update week 4

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view("welcome");
+    return view("index");
 });
 
-Route::get('about', function () {
-    return view("about");
-});
+Route::get('abouts', function () {
+    $name = "Tanakorn";
+    $date = "6 ก.ค. 2569";
+    return view("abouts" ,compact('name','date'));
+})->name("abouts");
 
-Route::get('blog', function () {
-    return view("blog");
-});
+Route::get('blogs', function () {
+    $blogs=[
+    ['title' => 'บทความที่1','content'=> 'เนื้อหาบทความที่1','status' => true],
+    ['title' => 'บทความที่2','content'=> 'เนื้อหาบทความที่2','status' => true],
+    ['title' => 'บทความที่3','content'=> 'เนื้อหาบทความที่3','status' => true],
+];
+
+    return view("blogs" , compact('blogs'));
+})->name("blogs");
 
